6f0a71e320b6729c6d165465369a1b2fa2e98bf1
Max Fresonke <max@flocksafety.com>
2023-10-11 18:05:43 -0400

    add attl options to work config

--- local_configs/zconfig-Max Fresonke HGWTXYD2RD.sh
+++ local_configs/zconfig-Max Fresonke HGWTXYD2RD.sh
@@ -1,3 +1,62 @@
 # Max's Flock MBP Settings
 
+alias uuid="uuidgen | tr '[:upper:]' '[:lower:]' | tee >(tr -d '\n' | pbcopy)"
+alias iso="node -e 'console.log(new Date().toISOString())' | tee >(tr -d '\n' | pbcopy)"
+alias cdp='cd ~/Projects'
+alias pr='gh pr create --web'
+alias c='code .'
+
+unalias gc
+gc() {
+  local branch_name="$1"
+
+  # Check if a branch with the given name already exists locally
+  if git rev-parse --verify --quiet "refs/heads/$branch_name"; then
+    git checkout "$branch_name"
+    echo "Checked out existing local branch: $branch_name"
+  else
+    # Check if a branch with the given name exists remotely
+    if git show-ref --verify --quiet "refs/remotes/origin/$branch_name"; then
+      git checkout -t "origin/$branch_name"
+      echo "Created and checked out a new tracking branch: $branch_name"
+    else
+      read -q "REPLY?The branch '$branch_name' does not exist locally or remotely. Do you want to create a new local branch? (y/n): "
+      echo
+      if [[ "$REPLY" = [yY] ]]; then
+        git checkout -b "$branch_name"
+        echo "Created and checked out a new local branch: $branch_name"
+      else
+        echo "No branch was created."
+      fi
+    fi
+  fi
+}
+
 alias cabo='export PGPASSWORD=$(aws rds generate-db-auth-token --hostname prod-cabo.cc0b62yca12h.us-east-1.rds.amazonaws.com --port 5432 --region us-east-1 --username iam_role_ro --no-cli-auto-prompt); echo -n $PGPASSWORD | pbcopy; echo "cabo password copied to clipboard"'
+alias tanqueray='export PGPASSWORD=$(aws rds generate-db-auth-token --hostname prod-tanqueray-replica.cc0b62yca12h.us-east-1.rds.amazonaws.com --port 5432 --region us-east-1 --username iam_role_ro --no-cli-auto-prompt); echo -n $PGPASSWORD | pbcopy; echo "tanq-prod password copied to clipboard"'
+alias tanqueray_admin='export PGPASSWORD=$(aws rds generate-db-auth-token --hostname prod-tanqueray.cc0b62yca12h.us-east-1.rds.amazonaws.com --port 5432 --region us-east-1 --username iam_role_admin --no-cli-auto-prompt); echo -n $PGPASSWORD | pbcopy; echo "tanq-prod password copied to clipboard"'
+
+# Fix the cluster that is python
+eval "$(pyenv init -)"
+
+# NVM Stuff place this after nvm initialization!
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
